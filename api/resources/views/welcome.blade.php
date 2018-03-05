8c870863ec2abeab42b9dfc82f1693ee1856cd13
Tudor Rusu <tudor.rusu@hotmail.com>
2018-03-05 15:41:17 +0100

    welcome page refactored

---
+++ api/resources/views/welcome.blade.php
@@ -0,0 +1,81 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>DSU ReportApp API</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Raleway', sans-serif;
+                font-weight: 100;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+                text-align: left;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+
+            .title small{
+                float: left;
+                font-size: 38px;
+            }
+
+            .title small span {
+                display: block;
+                width: 100%;
+                float: left;
+                font-size: 18px;
+                padding-top: 110px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            <div class="content">
+                <div class="title m-b-md">
+                    DSU ReportApp API<br />
+                    <small>API dedicata aplicatiei de raportare DSU<br /><span>dezvoltata cu {{ $version }}</span></small>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
